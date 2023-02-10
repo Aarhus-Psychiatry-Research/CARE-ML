317445316374b59b836f2dcfc45066e6855778d7
sarakolding <sarakolding@live.dk>
2023-02-10 17:50:19 +0100

    remove filtering by quarantine_df

--- application/main.py
+++ application/main.py
@@ -45,8 +45,6 @@ def main():
         prediction_times_df=LoadCoercion.coercion_df(timestamps_only=False),
         drop_pred_times_with_insufficient_look_distance=False,
         project_info=project_info,
-        quarantine_df=load_move_into_rm_for_exclusion(),
-        quarantine_days=None,
     )
 
     split_and_save_dataset_to_disk(
