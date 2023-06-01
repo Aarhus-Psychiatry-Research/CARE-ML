af58a02b4c06ee476439a67c590c60c37804959c
SigneKirkBrødbæk#5330 <signebroedbaek@gmail.com>
2023-06-01 08:54:58 +0200

    fix: model pipeline typehint

--- src/care_ml/feature_generation/modules/text_model_pipelines/fit_and_save_text_models.py
+++ src/care_ml/feature_generation/modules/text_model_pipelines/fit_and_save_text_models.py
@@ -5,15 +5,23 @@ from psycop.common.feature_generation.text_models.text_model_pipeline import (
 )
 
 if __name__ == "__main__":
-    model_names = [["bow"], ["tfidf"]]
 
-    for model in model_names:
-        text_model_pipeline(
-            model=model,
-            corpus_name="psycop_train_all_sfis_all_years_lowercase_stopwords_and_symbols_removed",
-            sfi_type=["Aktuelt psykisk"],  # Current Subjective Mental State
-            max_features=100,
-            max_df=1.0,
-            min_df=1,
-            ngram_range=(1, 2),
-        )
+    text_model_pipeline(
+        model="bow",
+        corpus_name="psycop_train_all_sfis_all_years_lowercase_stopwords_and_symbols_removed",
+        sfi_type=["Aktuelt psykisk"],  # Current Subjective Mental State
+        max_features=100,
+        max_df=1.0,
+        min_df=1,
+        ngram_range=(1, 2),
+    )
+
+    text_model_pipeline(
+        model="tfidf",
+        corpus_name="psycop_train_all_sfis_all_years_lowercase_stopwords_and_symbols_removed",
+        sfi_type=["Aktuelt psykisk"],  # Current Subjective Mental State
+        max_features=100,
+        max_df=1.0,
+        min_df=1,
+        ngram_range=(1, 2),
+    )
